0e47ca1fb1d669e109d8c3a1b1e5850700ef69ec
masatoshi kusano <uppleate@cordiary.net>
2022-06-07 10:48:19 +0900

    課題jsファイルの追加

---
+++ kadai①-2.js
@@ -0,0 +1,10 @@
+let japaneseCapital = prompt(`日本の首都は`);
+const answer = /^東京$|^東京都$|^とうきょう$|^とうきょうと$|^tokyo$/;
+
+while (answer.test(japaneseCapital) === false) {
+  alert(`不正解です`);
+  japaneseCapital = ``;
+  japaneseCapital = prompt(`日本の首都は`);
+}
+
+alert(`正解です`);
