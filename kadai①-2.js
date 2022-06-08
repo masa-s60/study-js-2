84c487f371dbc9a59c0f33ef91c40b31249946d3
masatoshi kusano <uppleate@cordiary.net>
2022-06-08 14:20:17 +0900

    テンプレートリテラルをシングルクオーテーションに修正、及び不要コードの削除

--- kadai①-2.js
+++ kadai①-2.js
@@ -1,10 +1,9 @@
-let japaneseCapital = prompt(`日本の首都は`);
+let japaneseCapital = prompt('日本の首都は');
 const answer = /^東京$|^東京都$|^とうきょう$|^とうきょうと$|^tokyo$/;
 
 while (answer.test(japaneseCapital) === false) {
-  alert(`不正解です`);
-  japaneseCapital = ``;
-  japaneseCapital = prompt(`日本の首都は`);
+  alert('不正解です');
+  japaneseCapital = prompt('日本の首都は');
 }
 
-alert(`正解です`);
+alert('正解です');
